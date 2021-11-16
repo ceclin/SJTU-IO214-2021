aec4fd0fbce2907edb6064a054fbddbeb0aaac3b
Cecil Lin <iceclin@outlook.com>
2021-11-17 03:56:52 +0800

    feat: add total cost in solution output

--- src/solution.cpp
+++ src/solution.cpp
@@ -7,10 +7,13 @@ std::ostream &operator<<(std::ostream &os, const Solution &soln)
     os << "Solution to VRP with " << vrp.locations.size() - 1 << " client locations and "
        << vrp.vehicle_types.size() << " vehicle types:" << '\n';
     const auto &vehicles = soln.vehicles;
+    double cost = 0.0;
     for (size_t i = 0; i < vehicles.size(); i++)
     {
         os << "Vehicle #" << (i + 1) << ' ' << vehicles[i] << '\n';
+        cost += vehicles[i].cost();
     }
+    os << "\ntotal cost = " << cost << '\n';
     os << "------\n";
     return os;
 }
